4d82af84e11259b546b20fae528aa086878cc5c7
natakamm <natakamm1@googlemail.com>
2024-08-01 19:50:50 +0200

    created basic component and functionality for modal of entries

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,15 @@
-const Card = ({ entry }) => {
+const Card = ({ entry, onOpenModal }) => {
   //function to show modal
-  return <div>Card</div>;
+  return (
+    <div>
+      <h3>{entry.title}</h3>
+      <p>{entry.date}</p>
+      <img src={entry.image} alt={entry.title} />
+      <button className="btn" onClick={() => onOpenModal(entry)}>
+        View Details
+      </button>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/CardDetailsModal.jsx
+++ src/components/CardDetailsModal.jsx
@@ -1,5 +1,22 @@
-const CardDetailsModal = () => {
-  return <div>CardDetailsModal</div>;
+const CardDetailsModal = ({ entry, closeModal }) => {
+  //when entry (props for selectedEntry value is false, then it shows nothing. It is false when its null, undefined, 0, NaN, "" (empty string), and false
+  if (!entry) return null;
+
+  return (
+    <div>
+      <dialog id="my_modal_1" className="modal" open>
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">{entry.title}</h3>
+          <p className="py-4">{entry.content}</p>
+          <div className="modal-action">
+            <button className="btn" onClick={closeModal}>
+              Close
+            </button>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
 };
 
 export default CardDetailsModal;
