1767c84fcb36c1f25f989e53fdfadd7c3914c81d
Deancs1 <carmel.dean+GitHub@gmail.com>
2024-08-02 19:10:01 +0200

    finished card view on main page and in the full card view

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,30 @@
+import defaultImage from "../assets/Book Yellow.webp";
+
 const Card = ({ entry, onOpenModal }) => {
   //function to show modal
   return (
-    <div className="bg-primary p-5">
-      <h3>{entry.title}</h3>
-      <p>{entry.date}</p>
-      <img src={entry.image} alt={entry.title} />
-      <button className="btn" onClick={() => onOpenModal(entry)}>
-        View Details
-      </button>
+    <div className="flex justify-center my-4">
+      <div className="card glass overflow-hidden max-w-sm">
+        <figure className="relative w-full h-48 bg-gray-300 ">
+          <img
+            src={entry.image || defaultImage}
+            alt={entry.title}
+            className=""
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{entry.title}</h2>
+          <p>{entry.date}</p>
+          <div className="card-actions justify-end">
+            <button
+              className="p-4 bg-primary hover:bg-secondary rounded-xl"
+              onClick={() => onOpenModal(entry)}
+            >
+              View Details
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
